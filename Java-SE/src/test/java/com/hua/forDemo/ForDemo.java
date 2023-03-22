fd40d4685ed695cdd879c810077642f26d9231d5
huawei <fuhuawei1997@163.com>
2023-03-22 15:14:31 +0800

    1、添加线程池。2、添加通过ssh连接Shell

--- Java-SE/src/test/java/com/hua/forDemo/ForDemo.java
+++ Java-SE/src/test/java/com/hua/forDemo/ForDemo.java
@@ -2,10 +2,14 @@ package com.hua.forDemo;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ForDemo {
     //1
     @Test
-    public void testContinue(){
+    public void testContinue() {
         for (int i = 0; i < 5; i++) {
             if (i == 3)
                 continue;
@@ -15,7 +19,7 @@ public class ForDemo {
 
     //2
     @Test
-    public void test2(){
+    public void test2() {
         long begin = System.currentTimeMillis();
         for (int i = 1; i <= 100000; i++) {
             int j = i * 16;
@@ -25,9 +29,10 @@ public class ForDemo {
         System.out.println();
         System.out.println("耗费时间：" + (end - begin));
     }
-    //3
+
+    //3耗费时间
     @Test
-    public void test3(){
+    public void test3() {
         long begin = System.currentTimeMillis();
         for (int i = 1; i <= 100000; i++) {
             int j = i << 4;
@@ -36,5 +41,33 @@ public class ForDemo {
         long end = System.currentTimeMillis();
         System.out.println();
         System.out.println("耗费时间：" + (end - begin));
+    }
+
+    //
+    @Test
+    public void testForSleep() {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        //计数器（循环次数）
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+
+        for (int i = 0; i < 5; i++) {
+            int finalI = i;
+            executorService.submit(() -> {
+                try {
+                    System.out.println("执行第" + finalI + "次 start");
+                    Thread.sleep(3000);
+                    System.out.println("执行第" + finalI + "次 end");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            countDownLatch.countDown();
+        }
+        //阻塞线程，直到调用N次，countDown()方法才释放线程
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ Java-SE/src/test/java/com/hua/shell/DemoShell.java
@@ -0,0 +1,20 @@
+package com.hua.shell;
+
+import org.junit.Test;
+
+public class DemoShell {
+    //测试shell
+    @Test
+    public void testShell(){
+        try {
+            RemoteShellExecutor se = new RemoteShellExecutor("39.106.139.198","root","yunkeyo123456!");
+
+            String exec = se.exec("sh /tmp/a.sh");
+            System.out.println(exec);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
